229e3504353b8da1e007769cdafb0293b57f42a5
Steven Vandegrift <svandegrift@icloud.com>
2023-11-18 22:43:29 -0600

    Code Repeats, Update Requirements, Update Markdown

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ Free Youtube to MP3 Converter
 ### Requirements
 - [Python3](https://www.python.org/downloads/)
   
-<i> Mac users need to go to Applications/Python3.x/ and select Install Certificates.command<i>
+> <i> Mac users need to go to Applications/Python3.x/ and select Install Certificates.command </i>
 
-I got tired of using those websites with tons of ads and misleading download links, so I made a script myself
+I got tired of using those websites with tons of ads and misleading download links, so I made a script myself. Enjoy!
+
+## How To
+1. Navigate to the TubeTunes folder in terminal
+2. Type `pip install -r requirements.txt` into the terminal
+3. Type `Python3 TubeTunes.py`
 
 
--- TubeTunes.py
+++ TubeTunes.py
@@ -1,15 +1,33 @@
-from pytube import YouTube
+import pytube
 import os
+
+isRunning = True
 
 downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
 
+# Download Youtube Audio
 def Download_Youtube_Audio(link):
-    yt = YouTube(link)
+    yt = pytube.YouTube(link)
+    print("Downloading...")
     audio = yt.streams.filter(only_audio=True).first()
     audio.download(filename=f"{yt.title}.mp3", output_path=downloads_folder)
 
-youtube_link = input('Enter Youtube Link: ')
-try:
-    Download_Youtube_Audio(youtube_link)
-finally:
-    print("Youtube Audio Downloaded Successfully")
+#Program Welcome Prompt
+print("TubeTunes")
+print("Youtube to mp3 converter")
+print('To close program, hit option C or type "quit"')
+
+while isRunning:
+    responce = input('Enter Youtube Link: ')
+    if responce == 'quit':
+        isRunning = False
+        break
+    else:
+        try:
+            Download_Youtube_Audio(responce)    
+            print("Youtube Audio Downloaded Successfully")
+        except Exception as e:
+            if isinstance(e, pytube.exceptions.RegexMatchError):
+                print('Error: Link is not a valid Youtube link. Try Again.')
+            else:
+                print(f"Error: {type(e)}")            
