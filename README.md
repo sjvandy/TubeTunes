5db5473e380f98f63281ab987dc45328482da482
Steven Vandegrift <svandegrift@icloud.com>
2023-11-20 15:40:40 -0600

    Create Standalone Appication

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
 # TubeTunes
 Free Youtube to MP3 Converter
 
-### Requirements
-- [Python3](https://www.python.org/downloads/)
-  
-> <i> Mac users need to go to Applications/Python3.x/ and select Install Certificates.command </i>
-
 I got tired of using those websites with tons of ads and misleading download links, so I made a script myself. Enjoy!
 
-## How To
-1. Navigate to the TubeTunes folder in terminal
-    - Copy Pathfile
-    - Open Terminal
-    - Type `cd ~pathfile_name`
-2. Type `pip3 install -r requirements.txt` into the terminal
-3. Type `Python3 TubeTunes.py`
-4. Enter Link to Youtube video
-5. Mp3 will download to the user downloads folder
+## Update
+I created a user interface to make the process more streamline as well as a App! Enjoy
+![Interface](./ui.png)
 
 ###### Note
 Audio Files downloaded from youtube cannot be added to Apple Music or other audio platforms. 
-Best use is for Discord soundboard
+Best use is for Discord soundboard or taking audio file into a DAW and re-exporting it
--- TubeTunes.py
+++ TubeTunes.py
@@ -1,35 +1,59 @@
 import os
 from pytube import exceptions
 from pytube import YouTube
+import tkinter as tk
 
-# Allows user to download multiple audio clips without restarting program
-isRunning = True
-
+#Declaring User's Download Folder
 downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
 
+def destroy_label(label):
+    label.destroy()
+
 # Download Youtube Audio
-def Download_Youtube_Audio(link):
-    yt = YouTube(link)
-    print("Downloading...")
+def download_youtube_audio(link):
+    yt = YouTube(link)    
     audio = yt.streams.filter(only_audio=True).first()
-    audio.download(filename=f"{yt.title}.mp3", output_path=downloads_folder)
+    audio.download(filename=f"{yt.title}.mp3", output_path=downloads_folder)    
 
-#Program Welcome Prompt
-print("TubeTunes")
-print("Youtube to mp3 converter")
-print('To close program, hit option C or type "quit"')
+# Define Button Click
+def button_pushed():   
+    user_input = textbox.get()
+    try:
+        download_youtube_audio(user_input)            
+        result_label.config(text='Download Successful!')                
+    except Exception as e:
+        if isinstance(e, exceptions.RegexMatchError):            
+            result_label.config(text="Link is not a valid Youtube link. Try Again.") 
+            print('Error Occured with Link')                
+        else:            
+            result_label.config(text=f"Error: {type(e)}")
+            print('Error Occured')               
+    
+def key_input(event):    
+    button_pushed()    
 
-while isRunning:
-    user_input = input('Enter Youtube Link: ')
-    if user_input == 'quit':
-        isRunning = False
-        break
-    else:
-        try:
-            Download_Youtube_Audio(user_input)    
-            print("Youtube Audio Downloaded Successfully")
-        except Exception as e:
-            if isinstance(e, exceptions.RegexMatchError):
-                print('Error: Link is not a valid Youtube link. Try Again.')
-            else:
-                print(f"An Error Occured: {type(e)}")            
+
+#GUI User Interface
+root = tk.Tk()
+
+root.geometry("500x300")
+root.title("TubeTunes")
+
+title = tk.Label(root, text="Youtube to MP3 Converter", font=('Helvetica', 18))
+title.pack(padx=20, pady=30)
+
+label = tk.Label(root, text="Enter a Youtube Link", font=('Helvetica', 14))
+label.pack(padx=20, pady=15)
+root.bind('<Return>', key_input)
+
+textbox = tk.Entry(root, width=40)
+textbox.pack(padx=10)
+
+button = tk.Button(root, text='Download MP3', font=('Helvetica', 18), height=3, command=button_pushed)
+button.pack(padx=20, pady=10)
+
+global result_label
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+root.mainloop()
