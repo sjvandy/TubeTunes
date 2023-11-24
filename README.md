5d7e68a72f67bdeecacef2f6a73714bfab1fbc32
Steven Vandegrift <svandegrift@icloud.com>
2023-11-24 02:49:35 -0600

    Simplify App to Terminal

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ Free Youtube to MP3 Converter
 
 I got tired of using those websites with tons of ads and misleading download links, so I made a script myself. Enjoy!
 
-## Update
-I created a user interface to make the process more streamline as well as a App!
-![Interface](./ui.png)
+## Updates
+Base on user experience, simplified app down to single terminal app since all you need to do is paste a link and hit enter.
 
-###### Note
-Audio Files downloaded from youtube cannot be added to Apple Music or other audio platforms. 
-Best use is for Discord soundboard or taking audio file into a DAW and re-exporting it
+### Future Updates
+- The ability to add playlists and have then all download into one folder
+
--- TubeTunes.py
+++ TubeTunes.py
@@ -1,11 +1,12 @@
 import os
 from pytube import exceptions
 from pytube import YouTube
-import tkinter as tk
 from moviepy.editor import *
 
 #Declaring User's Download Folder
 downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+is_running = True
 
 # Download Youtube Audio
 def download_youtube_audio(link):
@@ -22,46 +23,16 @@ def convert_to_mp3(title):
         print(e)
     os.remove(f"{os.curdir}/movie.mp4")
 
-
 # Define Button Click
-def button_pushed():   
-    user_input = textbox.get()
+print('Youtube to MP3 Converter')
+while is_running:   
+    user_input = input("Enter the Youtube Link: ")
     try:
+        print('Downloading...')
         download_youtube_audio(user_input)            
-        result_label.config(text='Download Successful!')                
+        print('Download Successful')               
     except Exception as e:
         if isinstance(e, exceptions.RegexMatchError):            
-            result_label.config(text="Link is not a valid Youtube link. Try Again.") 
-            print('Error Occured with Link')                
+            print("Link is not a valid Youtube link. Try Again.")         
         else:            
-            result_label.config(text=f"Error: {type(e)}")
-            print('Error Occured')               
-    
-def key_input(event):    
-    button_pushed()    
-
-
-#GUI User Interface
-root = tk.Tk()
-
-root.geometry("500x300")
-root.title("TubeTunes")
-
-title = tk.Label(root, text="Youtube to MP3 Converter", font=('Helvetica', 18))
-title.pack(padx=20, pady=30)
-
-label = tk.Label(root, text="Enter a Youtube Link", font=('Helvetica', 14))
-label.pack(padx=20, pady=15)
-root.bind('<Return>', key_input)
-
-textbox = tk.Entry(root, width=40)
-textbox.pack(padx=10)
-
-button = tk.Button(root, text='Download MP3', font=('Helvetica', 18), height=3, command=button_pushed)
-button.pack(padx=20, pady=10)
-
-global result_label
-result_label = tk.Label(root, text="")
-result_label.pack()
-
-root.mainloop()
+            print(f"Error: {type(e)}")                       
