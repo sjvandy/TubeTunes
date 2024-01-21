e76060e69703ef000d923898b141b906c7aef549
Steven Vandegrift <svandegrift@icloud.com>
2024-01-20 20:35:48 -0800

    Clean up code

--- TubeTunes.py
+++ TubeTunes.py
@@ -6,8 +6,6 @@ from urllib.request import urlretrieve
 #Declaring User's Download Folder
 downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-is_running = True
-
 # Download Youtube Audio
 def download_youtube_audio(link):
     yt = YouTube(link)
@@ -22,17 +20,23 @@ def download_youtube_audio(link):
     base, ext = os.path.splitext(out_file) 
     new_file = base + '.mp3'
     os.rename(out_file, new_file) 
+
+    # Download Youtube Tumbnail
+    filename_path = f"{downloads_folder}/{yt.title}.png"
+    thumbnail = urlretrieve(yt.thumbnail_url, filename_path)
     
 # Define Button Click
 print('Youtube to MP3 Converter')
-while is_running:   
+while True:   
     user_input = input("Enter the Youtube Link: ")
-    try:
-        print('Downloading...')
-        download_youtube_audio(user_input)           
-        print('Download Successful')               
-    except Exception as e:
-        if isinstance(e, exceptions.RegexMatchError):            
-            print("Link is not a valid Youtube link. Try Again.")                     
-        else:            
-            print(f"Error: {e}")                       
+    if user_input == 'quit()': break
+    else:
+        try:
+            print('Downloading...')
+            download_youtube_audio(user_input)           
+            print('Download Successful')               
+        except Exception as e:
+            if isinstance(e, exceptions.RegexMatchError):            
+                print("Link is not a valid Youtube link. Try Again.")                     
+            else:            
+                print(f"Error: {e}")                       
